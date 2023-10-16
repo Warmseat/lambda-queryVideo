62b0ea38c4652ad482042093e04a2c9be31ac4ee
Quasark <q@quasark.xyz>
2023-10-16 16:27:18 -0700

    0.0.4: Q&A

--- index.js
+++ index.js
@@ -8,12 +8,46 @@ const ChatOpenAI = require("langchain/chat_models/openai").ChatOpenAI;
 // const Document = require("langchain/document").Document;
 
 exports.handler = async (event) => {
-  // TODO implement
   console.log('queryVideo v0.0.3')
-  console.log('heres the event!: ', event);
+  // console.log('heres the event!: ', event);
+
+  let youtubeURL = event.youtubeURL;
+  let userQuery = event.query;
+
+  const loader = YoutubeLoader.createFromUrl(youtubeURL, {
+  language: "en",
+  addVideoInfo: false,
+});
+
+// // Load the data  
+const data = await loader.load();
+
+const textSplitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 500,
+  chunkOverlap: 100,
+});
+
+// Split the the data into chunks
+const splitDocs = await textSplitter.splitDocuments(data);
+
+const vectorStore = await FaissStore.fromDocuments(
+  splitDocs,
+  new OpenAIEmbeddings()
+);
+
+const model = new ChatOpenAI({ modelName: "gpt-3.5-turbo" });
+const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever());
+
+const queryResponse = await chain.call({
+  query: userQuery,
+});
+
+
+
   const response = {
     statusCode: 200,
-    body: JSON.stringify('URL: ' + event.youtubeURL + '. Query: ' + event.query),
+    body: JSON.stringify('Answer ', queryResponse),
+    // body: JSON.stringify('URL: ' + youtubeURL + '. Query: ' + userQuery),
   };
   return response;
 };
